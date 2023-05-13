fabc49a73df8391444427e4bd7931c5b2e827d4f
aldogerard <aldogerard12@gmail.com>
2023-05-14 02:28:39 +0700

    add galery section & review section

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const menuToggleSpan = document.querySelectorAll(".menu-toggle span");
 
 const chooseCardsWrapper = document.querySelector(".choose-cards-wrapper ");
 const servicesCardsWrapper = document.querySelector(".services-cards-wrapper ");
+const reviewCardsWrapper = document.querySelector(".review-cards-wrapper ");
 
 menuToggle.addEventListener("click", () => {
   navbar.classList.toggle("slide");
@@ -58,6 +59,26 @@ const dataServices = [
     image: "./assets/2.jpg",
   },
 ];
+const dataReview = [
+  {
+    name: "John Smith",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, aliquam ipsum! Dicta ea impedit amet eius ullam libero unde non rem similique reprehenderit quo, aliquid voluptatibus fuga.",
+    image: "./assets/man.jpg",
+    quote: "./assets/quote.png",
+  },
+  {
+    name: "Lebron James",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, aliquam ipsum! Dicta ea impedit amet eius ullam libero unde non rem similique reprehenderit quo, aliquid voluptatibus fuga.",
+    image: "./assets/man.jpg",
+    quote: "./assets/quote.png",
+  },
+  {
+    name: "Eleanor Quinn",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, aliquam ipsum! Dicta ea impedit amet eius ullam libero unde non rem similique reprehenderit quo, aliquid voluptatibus fuga.",
+    image: "./assets/man.jpg",
+    quote: "./assets/quote.png",
+  },
+];
 
 const getDataCardsChoose = () => {
   let cards = [];
@@ -73,6 +94,14 @@ const getDataCardsServices = () => {
     cards += updateUISevices(res);
   });
   servicesCardsWrapper.innerHTML = cards;
+};
+
+const getDataCardsReview = () => {
+  let cards = [];
+  dataReview.map((res, i) => {
+    cards += updateUIReview(res, i);
+  });
+  reviewCardsWrapper.innerHTML = cards;
 };
 
 const updateUIChoose = (data) => {
@@ -100,5 +129,18 @@ const updateUISevices = (data) => {
 </div>`;
 };
 
+const updateUIReview = (data, i) => {
+  return `<div class="review-cards-container">
+  <div class="card-comment">
+    <div class="card-quote">
+    </div>
+    <p>${data.desc}</p>
+  </div>
+  <div class="card-person person-${i + 1}"></div>
+  <h4>${data.name}</h4>
+</div>`;
+};
+
 getDataCardsChoose();
 getDataCardsServices();
+getDataCardsReview();
