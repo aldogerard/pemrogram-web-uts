c6d5b63ffb7b34d190292a262f70e6733c6e8d05
aldogerard <aldogerard12@gmail.com>
2023-05-13 22:31:08 +0700

    add services section

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const title = document.querySelector(".navbar-title h1");
 const menuToggle = document.querySelector(".menu-toggle input");
 const menuToggleSpan = document.querySelectorAll(".menu-toggle span");
 
-const cardsWrapper = document.querySelector(".cards-wrapper ");
+const chooseCardsWrapper = document.querySelector(".choose-cards-wrapper ");
+const servicesCardsWrapper = document.querySelector(".services-cards-wrapper ");
 
 menuToggle.addEventListener("click", () => {
   navbar.classList.toggle("slide");
@@ -17,7 +18,7 @@ window.addEventListener("scroll", () => {
   return nav.classList.remove("scroll");
 });
 
-const data = [
+const dataChoose = [
   {
     title: "WE DELIVER QUALITY",
     desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recu, eaque. Fugit dolorem soluta repudiandae eos. consectetur",
@@ -40,15 +41,41 @@ const data = [
   },
 ];
 
-const getDataCards = () => {
+const dataServices = [
+  {
+    title: "CONSTRUCTION MANAGEMENT",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, aspernatur delectus. Eveniet, distinctio laboriosam eos dolor voluptatum architecto voluptas rerum cum vel expedita alias, ipsa sint ex!Consequatur, iure reprehenderit!",
+    image: "./assets/3.jpg",
+  },
+  {
+    title: "RENOVATION",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, aspernatur delectus. Eveniet, distinctio laboriosam eos dolor voluptatum architecto voluptas rerum cum vel expedita alias, ipsa sint ex!Consequatur, iure reprehenderit!",
+    image: "./assets/4.jpg",
+  },
+  {
+    title: "ARCHITECTURE",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, aspernatur delectus. Eveniet, distinctio laboriosam eos dolor voluptatum architecto voluptas rerum cum vel expedita alias, ipsa sint ex!Consequatur, iure reprehenderit!",
+    image: "./assets/2.jpg",
+  },
+];
+
+const getDataCardsChoose = () => {
   let cards = [];
-  data.map((res) => {
-    cards += updateUI(res);
+  dataChoose.map((res) => {
+    cards += updateUIChoose(res);
   });
-  cardsWrapper.innerHTML = cards;
+  chooseCardsWrapper.innerHTML = cards;
 };
 
-const updateUI = (data) => {
+const getDataCardsServices = () => {
+  let cards = [];
+  dataServices.map((res) => {
+    cards += updateUISevices(res);
+  });
+  servicesCardsWrapper.innerHTML = cards;
+};
+
+const updateUIChoose = (data) => {
   return `<div class="card-container">
   <div class="card-img">
     <img src=${data.image} alt="image">
@@ -58,4 +85,20 @@ const updateUI = (data) => {
 </div>`;
 };
 
-getDataCards();
+const updateUISevices = (data) => {
+  return `<div class="card-container">
+  <div class="card-img">
+    <img src=${data.image} alt="services">
+  </div>
+  <div class="card-container-desc">
+    <h6>${data.title}</h6>
+    <p>${data.desc}</p>
+    <a href="#" class="btn">
+      Know More
+    </a>
+  </div>
+</div>`;
+};
+
+getDataCardsChoose();
+getDataCardsServices();
