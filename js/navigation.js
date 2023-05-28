7a89e747569c78d6274aeefb78c3a60436b1f74d
aldogerard <aldogerard12@gmail.com>
2023-05-28 19:48:25 +0700

    add scroll-to-top

--- js/navigation.js
+++ js/navigation.js
@@ -4,13 +4,19 @@ const title = document.querySelector(".navbar-title h1");
 const menuToggle = document.querySelector(".menu-toggle input");
 const menuToggleSpan = document.querySelectorAll(".menu-toggle span");
 
+const scrollToTop = document.querySelector("div.scroll-container");
+
 menuToggle.addEventListener("click", () => {
   navbar.classList.toggle("slide");
 });
 
 window.addEventListener("scroll", () => {
   if (scrollY > 10) {
-    return nav.classList.add("scroll");
+    return nav.classList.add("scroll"), scrollToTop.classList.add("show-scroll-top");
   }
-  return nav.classList.remove("scroll");
+  return nav.classList.remove("scroll"), scrollToTop.classList.remove("show-scroll-top");
+});
+
+scrollToTop.addEventListener("click", () => {
+  document.documentElement.scrollTop = 0;
 });
